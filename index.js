9684024c8cfac5a813004444c74671a9c106c5de
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-14 10:00:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord=word.split("").reverse().join("")
+  if(word==reverseWord){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 }
+console.log(isPalindrome("mom"))
 
 /* 
   Add your pseudocode here
+  declare a fuction Palindrome
+  if the argument is palindrom it will return true
 */
 
 /*
   Add written explanation of your solution here
+  i will declare a valirible reverseWord
+  use split("") to split the word a return them in a array
+  the reverse the word which are split
+  use join("") to join them together
+  if they match with the original it will return true or false
 */
 
 // You can run `node index.js` to view these console logs
